4f5db902b0a22e8bd5eee03e06a5250977d89432
KeepZero <i@keepzero.net>
2014-01-08 20:32:02 +0800

    Add Get List shortcut

--- shortcut.go
+++ shortcut.go
@@ -25,21 +25,7 @@ func (r *R) GetInt64(k string) (int64, error) {
 		return int64(0), err
 	}
 
-	// switch type
-	switch intVal := rData.(type) {
-	case int64:
-		return intVal, nil
-	case string:
-		return strconv.ParseInt(intVal, 10, 64)
-	case []byte:
-		return strconv.ParseInt(fmt.Sprintf("%s", string(intVal)), 10, 64)
-	case nil:
-		return int64(0), nil
-	case redis.Error:
-		return int64(0), intVal
-	default:
-		return int64(0), errors.New("redis command return type not unknow")
-	}
+	return convertToInt64(rData)
 }
 
 func (r *R) GetString(k string) (str string, err error) {
@@ -64,5 +50,62 @@ func (r *R) GetString(k string) (str string, err error) {
 		return "", strVal
 	default:
 		return "", errors.New("redis command return type not unknow")
+	}
+}
+
+func (r *R) GetInt64List(k string, start int, stop int) ([]int64, error) {
+
+	ret := []int64{}
+	rDatas, err := r.Do("LRANGE", k, start, stop)
+	if err != nil || rDatas == nil {
+		return ret, err
+	}
+
+	if datas, ok := rDatas.([]interface{}); ok {
+		for _, data := range datas {
+			i, err := convertToInt64(data)
+			if err != nil {
+				return ret, err
+			}
+			ret = append(ret, i)
+		}
+	} else {
+		return ret, errors.New("redis command return type unknow")
+	}
+
+	return ret, nil
+}
+
+func (r *R) GetIntList(k string, start int, stop int) ([]int, error) {
+
+	ret := []int{}
+	retInt64, err := r.GetInt64List(k, start, stop)
+	if err != nil {
+		return ret, err
+	}
+
+	for _, i := range retInt64 {
+		ret = append(ret, int(i))
+	}
+
+	return ret, nil
+}
+
+func convertToInt64(val interface{}) (int64, error) {
+
+	// switch type
+	switch intVal := val.(type) {
+	case int64:
+		return intVal, nil
+	case string:
+		return strconv.ParseInt(intVal, 10, 64)
+	case []byte:
+		return strconv.ParseInt(fmt.Sprintf("%s", string(intVal)), 10, 64)
+	case nil:
+		return 0, nil
+	case redis.Error:
+		return 0, intVal
+	default:
+		return 0, errors.New("redis command return type unknow")
 	}
 }
