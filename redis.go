da60d748f5c883b90ceefabd038a21e4a57b08ec
KeepZero <i@keepzero.net>
2014-03-18 13:39:30 +0800

    Change to use redigo Pool

--- redis.go
+++ redis.go
@@ -2,58 +2,58 @@ package redisconn
 
 import (
 	"github.com/garyburd/redigo/redis"
-	"sync"
+	"time"
 )
 
 type R struct {
-	network string
-	address string
-	conn    redis.Conn
-
-	mu sync.Mutex
+	pool *redis.Pool
 }
 
-func (r *R) reconnect() (err error) {
+// Open a new *R
+func Open(network, address, password string) (*R, error) {
 
-	// reconnect if conn is nil
-	if r.conn != nil {
-		if r.conn.Err() != nil {
-			// close old conn
-			r.conn.Close()
-			r.conn, err = redis.Dial(r.network, r.address)
-		}
-	} else {
-		r.conn, err = redis.Dial(r.network, r.address)
+	// Test connection
+	_, err := redis.Dial(network, address)
+
+	// redigo pool
+	pool := &redis.Pool{
+		MaxIdle:     3,
+		IdleTimeout: 240 * time.Second,
+		Dial: func() (redis.Conn, error) {
+			c, err := redis.Dial(network, address)
+			if err != nil {
+				return nil, err
+			}
+			if password != "" {
+				if _, err := c.Do("AUTH", password); err != nil {
+					c.Close()
+					return nil, err
+				}
+			}
+			return c, err
+		},
+		TestOnBorrow: func(c redis.Conn, t time.Time) error {
+			_, err := c.Do("PING")
+			return err
+		},
 	}
-	return
-}
 
-func Open(network, address string) (*R, error) {
-
-	redis, err := redis.Dial(network, address)
-
+	// Return *R
 	return &R{
-		network: network,
-		address: address,
-		conn:    redis,
+		pool: pool,
 	}, err
 }
 
+// Do something
 func (r *R) Do(cmd string, args ...interface{}) (reply interface{}, err error) {
 
-	// Lock
-	r.mu.Lock()
-	defer r.mu.Unlock()
+	conn := r.pool.Get()
+	defer conn.Close()
 
-	// reconnect before use
-	if err = r.reconnect(); err != nil {
-		return nil, err
-	}
-
-	return r.conn.Do(cmd, args...)
+	return conn.Do(cmd, args...)
 }
 
-// TODO
-// func Send
-// func Receive
-// func Flush
+// get a redis connection
+func (r *R) Get() redis.Conn {
+	return r.pool.Get()
+}
